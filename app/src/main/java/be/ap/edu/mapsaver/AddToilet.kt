6d1f7c79d7744c7de390afb29b76d739e4606b6d
Gie "Max" Vanommeslaeghe <gievanom@hotmail.com>
2022-12-05 10:44:49 +0100

    Add new activity for adding toilets

---
+++ app/src/main/java/be/ap/edu/mapsaver/AddToilet.kt
@@ -0,0 +1,11 @@
+package be.ap.edu.mapsaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddToilet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_toilet)
+    }
+}
