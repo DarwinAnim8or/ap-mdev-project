30ca8caf7c3113bf7a5f3292c78ab8e2f10bd7b8
JonasStaes <94199691+JonasStaes@users.noreply.github.com>
2022-12-10 12:35:14 +0100

    updated list_view

--- app/src/main/java/be/ap/edu/mapsaver/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/be/ap/edu/mapsaver/MyItemRecyclerViewAdapter.kt
@@ -1,12 +1,8 @@
 package be.ap.edu.mapsaver
 
-import android.content.ContentValues.TAG
 import android.content.Context
-import android.nfc.Tag
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -41,9 +37,10 @@ class MyItemRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val fm: FragmentManager = (context as AppCompatActivity).supportFragmentManager
         val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.omschrijving
-        holder.detailView.text = item.openingsuren
+        holder.omschrijvingView.text = item.omschrijving
+        holder.openingsurenView.text = "${if(item.openingsuren == "null") "24 uur / 24 uur" else "${item.openingsuren}"}"
+        holder.betalendView.text = "Betalend: ${if(item.betalend == "null") "nee" else "${item.betalend}"}"
+        holder.afstandView.text = ""
 
         holder.itemView.setOnClickListener {
             fm.popBackStackImmediate()
@@ -53,18 +50,14 @@ class MyItemRecyclerViewAdapter(
 
     override fun getItemCount(): Int = values.size
 
-    public fun refreshDataset() {
-        notifyDataSetChanged()
-        Log.d(TAG, "test")
-    }
-
     inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-        val detailView: TextView = binding.detail
+        val omschrijvingView: TextView = binding.omschrijving
+        val openingsurenView: TextView = binding.openingsuren
+        val betalendView: TextView = binding.betalend
+        val afstandView: TextView = binding.afstand
 
         override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            return super.toString() + " '" + openingsurenView.text + "'"
         }
     }
 
